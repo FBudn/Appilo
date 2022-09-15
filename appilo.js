d375cafc6cd09ad43a53351e4ec079811e59b4ef
FBudn <111660524+FBudn@users.noreply.github.com>
2022-09-15 16:57:16 +0200

    added-scroll-animations-and-page-max-width

--- appilo.js
+++ appilo.js
@@ -100,3 +100,24 @@ const pFQS8 = document.querySelector("#p8");
 FQSbutton8.addEventListener("click", function () {
   pFQS8.classList.toggle("showp");
 });
+
+/* scrolling animations */
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 80;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
